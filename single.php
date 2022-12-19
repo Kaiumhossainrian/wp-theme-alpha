67beda8f39f4251ae018c0a5b8376fc12203b468
Md Kaium Hossain Molla <rian.nits90@gmail.com>
2022-12-19 18:40:31 +0600

    dd

--- single.php
+++ single.php
@@ -1,62 +1,68 @@
 <?php get_header();?>
 <body <?php body_class( );?>>
 <?php get_template_part("hero"); ?>
-<div class="posts">
-    <?php
-    while(have_posts( )):
-        the_post( );
-    ?>
 
-    <div class="post" <?php post_class(); ?>>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                <h2 class="post-title text-center"><?php the_title(); ?></h2>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="posts">
+                <?php
+                while(have_posts( )):
+                    the_post( );
+                ?>
+
+                <div class="post" <?php post_class(); ?>>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                            <h2 class="post-title"><?php the_title(); ?></h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>
+                                    <strong><?php the_author(); ?></strong><br/>
+                                    <?php echo get_the_date(); ?>
+                                </p>
+                            </div>
+                            <div class="col-md-12">
+                                <p>
+                                <?php
+                                    if(has_post_thumbnail( )){
+                                        the_post_thumbnail("large", array("class"=>"img-fluid"));
+                                    }
+                                ?>
+                                </p>
+                                <p>
+                                    <?php 
+                                    the_content( );
+
+                                    next_post_link(  );
+                                    echo "<br>";
+                                    previous_post_link();
+                                    ?>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <?php /* if(comments_open( )){ ?>
+                            <div class="col-lg-10">
+                                <?php comments_template( ); ?>
+                            </div>
+                        <?php } */?> 
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-10 text-center">
-                    <p>
-                        <strong><?php the_author(); ?></strong><br/>
-                        <?php echo get_the_date(); ?>
-                    </p>
-                </div>
-                <div class="col-md-10">
-                    <p>
-                    <?php
-                        if(has_post_thumbnail( )){
-                            the_post_thumbnail("large", array("class"=>"img-fluid"));
-                        }
-                    ?>
-                    </p>
-                    <p>
-                        <?php 
-                           the_content( );
-                        ?>
-                    </p>
-                </div>
-            </div>
-            <div class="row">
-                <?php if(comments_open( )){ ?>
-                <div class="col-lg-10">
-                    <?php comments_template( ); ?>
-                </div>
-               <?php } ?> 
+
+                <?php
+                endwhile;
+                ?>
+                
             </div>
         </div>
+        <div class="col-lg-4"></div>
     </div>
-
-    <?php
-    endwhile;
-    ?>
-
-    <div class="container pb-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 text-center">
-                <?php the_posts_pagination(  ) ?>
-            </div>
-        </div>
-    </div>
-    
 </div>
+
+
 <?php get_footer();?>
